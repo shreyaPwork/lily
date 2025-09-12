766f595ac426618d31b2ff6f62043e6dd457b846
shreyaPwork <shreya.p@syntellite.com>
2025-09-12 19:53:33 +0530

    Added Why-Lily section and updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import { CustomerLayout } from '@/components/layouts/customer-layout'
 import { Hero } from '@/components/sections/hero'
 import { FeaturedProducts } from '@/components/sections/featured-products'
 import { Categories } from '@/components/sections/categories'
-
+import WhyLily from "@/components/sections/why-lily";
 export default function HomePage() {
   return (
     <CustomerLayout>
       <Hero />
       <FeaturedProducts />
+      <WhyLily />
       <Categories />
     </CustomerLayout>
   )
